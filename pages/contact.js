5ac08220f3c910dd54dcea3af79511cf54c3871c
RafaelDubois <rafaeldubois974@gmail.com>
2022-02-04 11:34:10 +0100

    final commit ?

--- pages/contact.js
+++ pages/contact.js
@@ -2,49 +2,55 @@ import React, { useState } from 'react'
 import {
   Box,
   Container,
-  Heading,
-  Image,
-  SimpleGrid,
   useColorModeValue,
-  Link,
   Button,
   FormLabel,
   Textarea,
   Input
 } from '@chakra-ui/react'
-import Section from '../components/section'
 import Layout from '../components/layouts/article'
-import Paragraph from '../components/paragraph'
-import NextLink from 'next/link'
+import { useAlert } from 'react-alert'
 
 const Contact = () => {
-  const [status, setStatus] = useState('Submit')
-  const handleSubmit = async e => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+  const alert = useAlert()
+  
+  const handleSubmit = e => {
     e.preventDefault()
-    setStatus('Sending...')
-    const { name, email, message } = e.target.elements
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value
+    console.log('Sending')
+    let data = {
+      name,
+      email,
+      message
     }
-    let response = await fetch('http://localhost:5000/contact', {
+    fetch('/api/contactapi', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(details)
+      body: JSON.stringify(data)
+    }).then(res => {
+      console.log('Response received')
+      if (res.status === 200) {
+        alert.show('Message successfully sent')
+        console.log('Response succeeded!')
+        setSubmitted(true)
+        setName('')
+        setEmail('')
+        setMessage('')
+      }
     })
-    setStatus('Submit')
-    let result = await response.json()
-    alert(result.status)
   }
+
   return (
     <Layout>
       <Container delay={5}>
         <form onSubmit={handleSubmit}>
           <Box
-          
             borderRadius="lg"
             mt={2}
             mb={6}
@@ -52,19 +58,46 @@ const Contact = () => {
             textAlign="center"
             bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
           >
-            
-              <FormLabel htmlFor="name" mt={2} ml={2}>Name:</FormLabel>
-              <Input type="text" id="name" required />
-            
-            
-              <FormLabel htmlFor="email" mt={2} ml={2}>Email:</FormLabel>
-              <Input type="email" id="email" required />
-            
-            
-              <FormLabel htmlFor="message" mt={2} ml={2}>Message:</FormLabel>
-              <Textarea id="message" required />
-            
-            <Button type="submit"mt={6} mb={2}>{status}</Button>
+            <FormLabel htmlFor="name" mt={2} ml={2}>
+              Name:
+            </FormLabel>
+            <Input
+              type="text"
+              id="name"
+              required
+              placeholder="Name.."
+              onChange={e => {
+                setName(e.target.value)
+              }}
+            />
+
+            <FormLabel htmlFor="email" mt={2} ml={2}>
+              Email:
+            </FormLabel>
+            <Input
+              type="email"
+              id="email"
+              required
+              placeholder="Email.."
+              onChange={e => {
+                setEmail(e.target.value)
+              }}
+            />
+
+            <FormLabel htmlFor="message" mt={2} ml={2}>
+              Message:
+            </FormLabel>
+            <Textarea
+              id="message"
+              required
+              onChange={e => {
+                setMessage(e.target.value)
+              }}
+            />
+
+            <Button type="submit"  mt={6} mb={2} onClick={(e)=>{handleSubmit(e)}}>
+              Submit
+            </Button>
           </Box>
         </form>
       </Container>
